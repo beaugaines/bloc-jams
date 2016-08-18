3d08c3ec61cd3ab5f2f9c76212502fefaa4f8c06
Charlie Gaines <beaugaines@gmail.com>
2016-08-18 15:29:57 -0400

    forEach utility method working

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,25 @@
 'use strict';
 
-var animatePoints = function(points) {
-  for (var i = 0; i < points.length; i++) {
-    points[i].style.opacity = 1;
-    points[i].style.transform = 'scaleX(1) translateY(0)';
-    points[i].style.msTransform = 'scaleX(1) translateY(0)';
-    points[i].style.WebkitTransform = 'scaleX(1) translateY(0)';
-  }
+var animatePoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = 'scaleX(1) translateY(0)';
+    point.style.msTransform = 'scaleX(1) translateY(0)';
+    point.style.WebkitTransform = 'scaleX(1) translateY(0)';
 };
 
 window.onload = function () {
   var pointsArray = document.getElementsByClassName('point');
 
   if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+    forEach(pointsArray, animatePoint)
   }
-  
+
   var sellingPoints = pointsArray[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-
   window.addEventListener('scroll', function () {
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+      forEach(pointsArray, animatePoint)
     }
   });
 
