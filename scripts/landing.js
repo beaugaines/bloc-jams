60d5b0f3a43ea1142fd361235b804a6f89bdb565
Charlie Gaines <beaugaines@gmail.com>
2016-08-18 14:25:20 -0400

    refactor landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,16 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-  (function() {
-    var points = document.getElementsByClassName('point');
-    for (var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = 'scaleX(1) translateY(0)';
-      points[i].style.msTransform = 'scaleX(1) translateY(0)';
-      points[i].style.WebkitTransform = 'scaleX(1) translateY(0)';
-    }
-  })();
+  var points = document.getElementsByClassName('point');
+
+  function revealPoint(i) {
+    points[i].style.opacity = 1;
+    points[i].style.transform = 'scaleX(1) translateY(0)';
+    points[i].style.msTransform = 'scaleX(1) translateY(0)';
+    points[i].style.WebkitTransform = 'scaleX(1) translateY(0)';
+  }
+
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
+  }
 });
