512ff6aa3694f1af3b5352a2cc107e9080d677de
Charlie Gaines <beaugaines@gmail.com>
2016-08-18 15:04:47 -0400

    checkpoint 23 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,29 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', function () {
-  (function() {
-    var points = document.getElementsByClassName('point');
-    for (var i = 0; i < points.length; i++) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = 'scaleX(1) translateY(0)';
-      points[i].style.msTransform = 'scaleX(1) translateY(0)';
-      points[i].style.WebkitTransform = 'scaleX(1) translateY(0)';
+var animatePoints = function(points) {
+  for (var i = 0; i < points.length; i++) {
+    points[i].style.opacity = 1;
+    points[i].style.transform = 'scaleX(1) translateY(0)';
+    points[i].style.msTransform = 'scaleX(1) translateY(0)';
+    points[i].style.WebkitTransform = 'scaleX(1) translateY(0)';
+  }
+};
+
+window.onload = function () {
+  var pointsArray = document.getElementsByClassName('point');
+
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  
+  var sellingPoints = pointsArray[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+
+  window.addEventListener('scroll', function () {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
     }
-  })();
-});
+  });
+
+};
