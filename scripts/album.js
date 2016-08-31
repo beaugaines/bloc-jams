03c4eb2e181f23885b7d673f97185e3a286a9878
Charlie Gaines <beaugaines@gmail.com>
2016-08-31 06:23:59 -0400

    checkpoint 31 complete

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,30 @@ var albumMarconi = {
   ]
 };
 
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+// does this need to be set to null?  Can't it just be declared?
+var currentlyPlayingSong = null;
+
+
+var clickHandler = function () {
+  var $songNumberCell = $(this).find('.song-item-number');
+
+  if(currentlyPlayingSong === null) {
+    $songNumberCell.html(pauseButtonTemplate);
+    currentlyPlayingSong = $songNumberCell.attr('data-song-number');
+  } else if (currentlyPlayingSong === $songNumberCell.attr('data-song-number')) {
+    $songNumberCell.html(playButtonTemplate);
+    currentlyPlayingSong = null;
+  } else if (currentlyPlayingSong !== $songNumberCell.attr('data-song-number')) {
+    var $currentlyPlayingSongElement = $('[data-song-number="' + currentlyPlayingSong + '"]');
+    $currentlyPlayingSongElement.html($currentlyPlayingSongElement.attr('data-song-number'));
+    $songNumberCell.html(pauseButtonTemplate);
+    currentlyPlayingSong = $songNumberCell.attr('data-song-number');
+  }
+};
+
+
 var createSongRow = function (songNumber, songName, songLength) {
   var template =
     '<tr class="album-view-song-item">' +
@@ -38,7 +62,32 @@ var createSongRow = function (songNumber, songName, songLength) {
     '  <td class="song-item-duration">' + songLength + '</td>' +
     '</tr>';
 
-  return $(template);
+  var $row =  $(template);
+
+  var onHover = function () {
+    var $songNumberCell = $(this).find('.song-item-number');
+    var songNumber = $songNumberCell.attr('data-song-number');
+
+    if (songNumber !== currentlyPlayingSong) {
+      $songNumberCell.html(playButtonTemplate);
+    }
+  };
+
+  var offHover = function () {
+    var $songNumberCell = $(this).find('.song-item-number');
+    var songNumber = $songNumberCell.attr('data-song-number');
+
+    if (songNumber !== currentlyPlayingSong) {
+      $songNumberCell.html(songNumber);
+    }
+  };
+
+  // you can attache event listners to dynamically created elements
+  // before adding them to the DOM
+  $row.click(clickHandler);
+  $row.hover(onHover, offHover);
+
+  return $row;
 };
 
 var setCurrentAlbum = function (album) {
@@ -54,7 +103,6 @@ var setCurrentAlbum = function (album) {
   $albumReleaseInfo.text(album.year + ' ' + album.label);
   $albumImage.attr('src', album.albumArtUrl);
 
-
   $albumSongList.empty();
 
   for (var i = 0; i < album.songs.length; i++) {
@@ -63,90 +111,7 @@ var setCurrentAlbum = function (album) {
   }
 };
 
-var findParentByClassName = function (element, className) {
-  if (element) {
-    var currentParent = element.parentElement;
-    while (currentParent.className !== className && currentParent.className !== null) {
-      currentParent = currentParent.parentElement;
-    }
-    return currentParent;
-  }
-};
 
-
-var getSongItem = function(element) {
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        case 'song-item-number':
-            return element;
-        default:
-            // is this default necessary?
-            return;
-    }
-};
-
-var clickHandler = function (targetElement) {
-  var songItem = getSongItem(targetElement);
-
-  if(currentlyPlayingSong === null) {
-    songItem.innerHTML = pauseButtonTemplate;
-    currentlyPlayingSong = songItem.getAttribute('data-song-number');
-  } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-    songItem.innerHTML = playButtonTemplate;
-    currentlyPlayingSong = null;
-  } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-    var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-    currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-    songItem.innerHTML = pauseButtonTemplate;
-    currentlyPlayingSong = songItem.getAttribute('data-song-number');
-  }
-};
-
-
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
-var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-
-
-// does this need to be set to null?  Can't it just be declared?
-var currentlyPlayingSong = null;
-
-window.onload = function () {
+$(document).ready(function () {
   setCurrentAlbum(albumPicasso);
-
-  songListContainer.addEventListener('mouseover', function (event) {
-    if (event.target.parentElement.className === 'album-view-song-item') {
-
-      var songItem = getSongItem(event.target);
-      var songItemNumber = songItem.getAttribute('data-song-number');
-
-      if (songItemNumber !== currentlyPlayingSong) {
-        songItem.innerHTML = playButtonTemplate;
-      }
-    }
-  });
-
-  for (var i = 0; i < songRows.length; i++) {
-    songRows[i].addEventListener('mouseleave', function (event) {
-      var songItem = getSongItem(event.target);
-      var songItemNumber = songItem.getAttribute('data-song-number');
-
-      if (songItemNumber !== currentlyPlayingSong) {
-        songItem.innerHTML = songItemNumber;
-      }
-    });
-
-    songRows[i].addEventListener('click', function (event) {
-      clickHandler(event.target);
-    })
-  }
-};
+});
